49a7955190c439afe7ac93a1765068be4ea3b10e
Aishwarya <paishwarya7777@msitprogram.net>
2022-07-11 15:12:25 +0530

    checkpoint1

---
+++ areaOfRectangle.py
@@ -0,0 +1,6 @@
+perimeter = int(input())     # perimeter of rectangle
+diff = int(input())         # difference between lenght and breadth
+b = int((perimeter - 2*diff)/4)
+a = diff + b
+print(a*b)
+# print the area of rectangle
